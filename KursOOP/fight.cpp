e53bbb9b47293b9f027f529fcdbf46bcff641061
Asquardian <alexromanov5262@gmail.com>
2020-11-02 11:54:15 +0700

    Fix generation

--- KursOOP/fight.cpp
+++ KursOOP/fight.cpp
@@ -5,17 +5,24 @@
 team fight(sf::RenderWindow &window) {
 	int healthSet[3], spiritSet[3], powerSet[3], minPowerSet[3], healthEnemy[3], powerEnemy[3], minPowerEnemy[3];
 	std::ifstream savedTeam("Save/TeamStat.save");
+	srand(time(NULL));
 	if (!savedTeam) {
 		exit(0);
 	}
 	for (int i = 0; i <= 2; i++) {
 		savedTeam >> healthSet[i];
 		healthEnemy[i] = rand()%healthSet[i];
+		if (healthEnemy[i] < 60)
+			healthEnemy[i] = 60;
 		savedTeam >> spiritSet[i];
 		savedTeam >> powerSet[i];
 		powerEnemy[i] = rand() % powerSet[i];
+		if (powerEnemy[i] < 50)
+			powerEnemy[i] = 50;
 		savedTeam >> minPowerSet[i];
 		minPowerEnemy[i] = rand() % minPowerSet[i];
+		if (minPowerEnemy[i] < 30)
+			minPowerEnemy[i] = 30;
 	}
 	team Player(healthSet, spiritSet, powerSet, minPowerSet);
 	enemy Shadow(healthEnemy, powerEnemy, minPowerEnemy);
