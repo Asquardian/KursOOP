e2a80a54c62d4944914001483701bc2a6a30df36
Asquardian <alexromanov5262@gmail.com>
2020-11-05 17:20:26 +0700

    Add cloud animation

--- KursOOP/menu.cpp
+++ KursOOP/menu.cpp
@@ -4,9 +4,13 @@ int menu(sf::RenderWindow& window) {
 	Button startGame(1246, 600, 100, 20, "Start");
 	Button upgrade(1246, 642, 140, 20, "Upgrade");
 	Button exit(1246, 684, 80, 20, "Exit");
-	sf::Sprite background;
-	sf::Texture backTexture;
+	sf::Sprite background, cloud;
+	cloud.setScale(0.5, 0.5);
+	sf::Texture backTexture, cloudTexture;
 	backTexture.loadFromFile("Asset/Menu.png");
+	cloudTexture.loadFromFile("Asset/cloud.png");
+	cloud.setTexture(cloudTexture);
+	float x = 0;
 	background.setTexture(backTexture);
 	while (window.isOpen()) {
 		sf::Event event;
@@ -16,6 +20,10 @@ int menu(sf::RenderWindow& window) {
 				window.close();
 		}
 		window.clear();
+		cloud.setPosition(x, 0);
+		x += 0.9;
+		if (x > 2000) 
+			x = -2200;
 		if (startGame.click(window) == true) 
 			return 0;
 		if (upgrade.click(window) == true)
@@ -23,6 +31,7 @@ int menu(sf::RenderWindow& window) {
 		if (exit.click(window) == true) 
 			return 2;
 		window.draw(background);
+		window.draw(cloud);
 		startGame.draw(window);
 		upgrade.draw(window);
 		exit.draw(window);
